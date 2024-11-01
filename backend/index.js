5bebf432432f8a5f3d0d35840e17e1bdc79ff2e8
atharva3149 <atharvanalawade3149@gmail.com>
2024-11-01 15:27:10 +0530

    changed get method

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 // MongoDB connection
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion , ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@yoga-master.b1dil.mongodb.net/?retryWrites=true&w=majority&appName=yoga-master`;
 
 const client = new MongoClient(uri, {
@@ -40,12 +40,87 @@ async function run() {
       res.send(result);
     });
 
-   
+   app.get('/classes', async(req, res)=>{
+     const query = { status : 'approved' };
+     const result = await classesCollection.find().toArray();
+     res.send(result); 
+   })
+
+  //  get classes by instructor email address
+    app.get('/classes/:email', async(req, res)=>{
+      const email = req.params.email;
+     const query = {instructorEmail : email};
+     const result = await classesCollection.find(query).toArray(); 
+     res.send(result); 
+    })
+
+    // manage classes 
+    app.get('/classes-manage', async(req, res)=>{
+      const result = await classesCollection.find().toArray();
+     res.send(result);
+    })
+
+    // update classes status and reason
+    app.patch('/change-status/:id', async(req, res)=>{
+      const id = req.params.id;
+      const status = req.body.status;
+      const reason = req.body.reason;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert: true } ;
+      const updateDoc = {
+        $set: {
+        status : status,
+        reason : reason,
+        },
+      };
+      const result = await classesCollection.updateOne(filter , updateDoc , options);
+      res.send(result);
+    })
+
+    // get approved classes
+    app.get('/approved-classes', async(req, res)=>{
+      const query = { status : 'approved' };
+      const result = await classesCollection.find(query).toArray();
+      res.send(result);
+    })
 
     // Test route
     app.get('/', (req, res) => {
       res.send('Hello Developers! 2025');
     });
+
+
+    // get single class details
+    app.get('/class/:id', async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)};
+      const result =  await classesCollection.findOne(query);
+      res.send(result);
+    })
+
+    // update class details (all data)
+    app.put('/update-class/:id',async (req,res)=>{
+      const id = req.params.id;
+      const updateClass = req.body;
+      const filter = {_id: new ObjectId(id)};
+      const options = { upsert:true};
+      const updateDoc = {
+        $set: {
+          name : updateClass.name,
+          description : updateClass.description,
+          price : updateClass.price,
+          availableSeats : parseInt(updateClass.availableSeats),
+          videoLink : updateClass.videoLink,
+          status : 'pending',
+
+        }
+      };
+      const result = await classesCollection.updateOne(filter, updateDoc, options);
+      res.send(result);
+
+    })
+
+
 
     // Confirm successful connection
     await client.db("admin").command({ ping: 1 });
